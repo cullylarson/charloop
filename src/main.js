89e4caf09206ec948c295778260d8c96db829819
Cully Larson <cully.larson@gmail.com>
2017-10-08 22:19:51 -0700

    Added some checks for whether on a raspi, so can run this app on other machines for dev.

--- src/main.js
+++ src/main.js
@@ -1,17 +1,30 @@
-const rpio = require('rpio')
+const readline = require('readline')
+const isPi = require('detect-rpi')
 const RotaryEncoder = require('./rotary-encoder')
 const Menu = require('./menu')
+const Bus = require('./bus')
 
-rpio.init({
-    gpiomem: true, /* Use /dev/gpiomem */
-    mapping: 'physical', /* Use the P1-P40 numbering scheme */
-})
+readline.emitKeypressEvents(process.stdin)
+process.stdin.setRawMode(true)
 
-const rotaryMain = RotaryEncoder(rpio, {
-    right: 11,
-    left: 13,
-    push: 15,
-})
+const rpio = isPi()
+    ? require('rpio')
+    : null
+
+if(isPi()) {
+    rpio.init({
+        gpiomem: true, /* Use /dev/gpiomem */
+        mapping: 'physical', /* Use the P1-P40 numbering scheme */
+    })
+}
+
+const rotaryMain = isPi()
+    ? RotaryEncoder(rpio, {
+        right: 11,
+        left: 13,
+        push: 15,
+    })
+    : Bus()
 
 const menu = Menu()
 
