8b28ce101f1d8d15846ca9cfcf4e603855893349
Cully Larson <cully.larson@gmail.com>
2017-10-25 22:43:10 -0700

    Added functions to the Bus to allow for subscribing to all events, and unsubscribing from events.

--- src/bus.js
+++ src/bus.js
@@ -1,14 +1,39 @@
 module.exports = function() {
     const cbs = {}
+    const allCbs = []
 
     return {
+        onAll: (cb) => {
+            allCbs.push(cb)
+        },
+
+        offAll: (cb) => {
+            const idx = allCbs.indexOf(cb);
+            if(idx === -1) return
+
+            // remove the callback
+            allCbs.splice(idx, 1)
+        },
+
         on: (label, cb) => {
             if(cbs.hasOwnProperty(label)) cbs[label].push(cb)
             else cbs[label] = [cb]
         },
 
+        off: (label, cb) => {
+            if(!cbs.hasOwnProperty(labe)) return
+
+            const idx = cbs[label].indexOf(cb);
+            if(idx === -1) return
+
+            // remove the callback
+            cbs[label].splice(idx, 1)
+        },
+
         trigger: (label, e) => {
             if(cbs.hasOwnProperty(label)) cbs[label].forEach(f => f(e))
+
+            if(allCbs.length) allCbs.forEach(f => f(label, e))
         },
     }
 }
